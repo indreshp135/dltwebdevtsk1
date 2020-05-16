242e5284203c5417b0e7116d2d8b897386c1a8b9
indreshp135 <indreshp135@gmail.com>
2020-05-16 22:07:33 +0530

    login fixed

--- javas/main.js
+++ javas/main.js
@@ -52,7 +52,7 @@ document.querySelector("#okl").onclick= function ()
   var fl=0;
   if(!(nAme==""))
   {
-    for(var i=0;i<player.length;i++)
+    for(var i=0;i<players.length;i++)
     {
       if(players[i].name==nAme)
       {
@@ -99,39 +99,50 @@ document.querySelector("#okl").onclick= function ()
         const end = start + Math.floor(Math.random() * 40)
         this.queue.push({ from, to, start, end })
       }
-    cancelAnimationFrame(this.frameRequest)
-    this.frame = 0
-    this.update()
-    return promise
-}
-update() 
-{
-    let output = ''
-    let complete = 0
-    for (let i = 0, n = this.queue.length; i < n; i++) {
-      let { from, to, start, end, char } = this.queue[i]
-      if (this.frame >= end) {
-        complete++
-        output += to
-      } else if (this.frame >= start) {
-        if (!char || Math.random() < 0.28) {
-          char = this.randomChar()
-          this.queue[i].char = char
+      cancelAnimationFrame(this.frameRequest)
+      this.frame = 0
+      this.update()
+      return promise
+    }
+    update() 
+    {
+      let output = ''
+      let complete = 0
+      for (let i = 0, n = this.queue.length; i < n; i++) 
+      {
+        let { from, to, start, end, char } = this.queue[i]
+        if (this.frame >= end)
+        {
+          complete++
+          output += to
+        } 
+        else if (this.frame >= start) 
+        {
+          if (!char || Math.random() < 0.28) 
+          {
+            char = this.randomChar()
+            this.queue[i].char = char
+          }
+          output += `<span class="dud">${char}</span>`
+        } 
+        else 
+        {
+          output += from
         }
-        output += `<span class="dud">${char}</span>`
-      } else {
-        output += from
+      }
+      this.el.innerHTML = output
+      if (complete === this.queue.length) 
+      {
+        this.resolve()
+      } 
+      else 
+      {
+        this.frameRequest = requestAnimationFrame(this.update)
+        this.frame++
       }
     }
-    this.el.innerHTML = output
-    if (complete === this.queue.length) {
-      this.resolve()
-    } else {
-      this.frameRequest = requestAnimationFrame(this.update)
-      this.frame++
-    }
-  }
-  randomChar() {
+  randomChar() 
+  {
     return this.chars[Math.floor(Math.random() * this.chars.length)]
   }
 }
@@ -141,8 +152,10 @@ const el = document.querySelector('.text');
 const fx = new TextScramble(el);
 
 let counter = 0;
-const next = () => {
-  fx.setText(phrases[counter]).then(() => {
+const next = () => 
+{
+  fx.setText(phrases[counter]).then(() => 
+  {
     setTimeout(next, 800)
   })
   counter = (counter + 1) % phrases.length
